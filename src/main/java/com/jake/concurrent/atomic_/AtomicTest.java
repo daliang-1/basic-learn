703d408b44170b7db37bd9bcdd7dd7637482a269
daliang-1 <wcl542309018@163.com>
2018-09-14 16:38:14 +0800

    atomic练习及底层原理分析

--- src/main/java/com/jake/concurrent/atomic_/AtomicTest.java
+++ src/main/java/com/jake/concurrent/atomic_/AtomicTest.java
@@ -35,6 +35,7 @@ public class AtomicTest {
     /**
      * 1.java.util.concurrent.atomic原子变量类,使用了Unsafe类的方法保证了并发安全性。
      *   1)数值对象使用volatile关键字修饰，保证多线程条件下可见性，任何一个线程修改了value值，会立马会写到主内存中，不会经过临时内存，保证了可见性
-     *   2)使用Unsafe.compareAndSwapInt(**)方法，即CAS(compare and swap)，CAS为底层的与操作系统相关的特性。
+     *   2)使用Unsafe.compareAndSwapInt(**)方法，即CAS(compare and swap)
+     *   3)依靠的便是Unsafe这个类中调用的该方法具有原子性，这个原子性的保证并不是靠java本身保证，而是靠一个更底层的与操作系统相关的特性实现
      */
 }
